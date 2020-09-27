bda7c9d8651b1a88c25ccbc3d68372bce4727b9e
diksha-kalra <kalradiksha11@gmail.com>
2020-09-27 21:56:44 +0530

    Added: Add contact details in address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,10 +4,66 @@ import java.util.*;
 
 public class AddressBookMain{
 	
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		
+		//Creating object of contactperson class
+		ContactPerson add = new ContactPerson();
+		int ch=0;
 		
 		//Printing welcome message
 		System.out.println("Welcome To Address Book Program");
-		
+		// while loop to take inputs from person
+		while(true) {
+			System.out.println("Enter 1 for add details");
+			System.out.println("Enter 2 for viewing all contact details");
+			System.out.println("Enter 0 to exit");
+			System.out.println("Enter the action to perform");
+			ch=obj.nextInt();
+		if(ch==1) {
+			PersonInfo p=new PersonInfo();
+			System.out.println("Enter First Name");
+			p.setFirst_name(obj.next());
+			System.out.println("Enter Last Name");
+			p.setLast_name(obj.next());
+			System.out.println("Enter Address");
+			p.setAddress(obj.next());
+			System.out.println("Enter City");
+			p.setCity(obj.next());
+			System.out.println("Enter State");
+			p.setState(obj.next());
+			System.out.println("Enter Zip Code");
+			p.setZip(obj.next());
+			System.out.println("Enter Phone Number");
+			p.setPhno(obj.next());
+			System.out.println("Enter Email");
+			p.setEmail(obj.next());
+			add.addPerson(p);
+			}
+		else if(ch==2) {
+			ArrayList<PersonInfo> result = add.viewAllContacts();
+			if(result.isEmpty()) {
+				System.out.println("List is empty");
+				continue;
+			}
+			Iterator<PersonInfo> iter=result.iterator();
+			while(iter.hasNext()) {
+				PersonInfo p=(PersonInfo)iter.next();
+				System.out.println("First Name-"+p.getFirst_name());
+				System.out.println("Last Name-"+p.getLast_name());
+				System.out.println("Address-"+p.getAddress());
+				System.out.println("City-"+p.getCity());
+				System.out.println("Statw-"+p.getState());
+				System.out.println("ZIP-"+p.getZip());
+				System.out.println("Phone Number-"+p.getPhno());
+				System.out.println("Email-"+p.getEmail());
+				}
+			}
+			else {
+				break;
+			}
+		}
 	}
 }
+
+
--- ContactPerson.java
+++ ContactPerson.java
@@ -20,67 +20,12 @@ import java.util.*;
 	}
 	
 	//Method to check if list is empty
-	public boolean isEmpty() {
-		return person.isEmpty();
-	}
+		public boolean isEmpty() {
+			return person.isEmpty();
+		}
 	
 	//Method to check all contacts available 
 	public ArrayList<PersonInfo> viewAllContacts(){
 		return person;
-	}
-	
-	//Method to remove contact details
-	public void remove(String name){
-        for(int i = 0; i < person.size(); i++){
-        	PersonInfo p = (PersonInfo)person.get(i); 
-        	if(name.equals(p.getFirst_name())){
-        		person.remove(i);
-        		System.out.println("Details of "+name+" removed");
-        	}
-           }
-		}
-	//Method to modify details
-     public void Modify(String name,int info_name){
-    	 for(int i = 0; i < person.size(); i++){
-             PersonInfo p = (PersonInfo)person.get(i); 
-             if(name.equals(p.getFirst_name())){
-            	 System.out.println("Modify Details");
-            	 switch(info_name) {
-            	   	case 1: 
-            		   System.out.println("Enter new First Name");
-            		   p.setFirst_name(obj.next());
-            		   break;
-            	   	case 2: 
-            	   		System.out.println("Enter new Last Name");
-           				p.setLast_name(obj.next());
-           				break;
-            	   	case 3: 
-            	   		System.out.println("Enter new Address");
-           				p.setAddress(obj.next());
-           				break;
-            	   	case 4: 
-            	   		System.out.println("Enter new City");
-           				p.setCity(obj.next());
-           				break;
-            	   	case 5:
-            	   		System.out.println("Enter new State");
-           				p.setState(obj.next());
-           				break;
-            	   	case 6:
-            	   		System.out.println("Enter new Zip Code");
-            	   		p.setZip(obj.next());
-            	   		break;
-            	   	case 7:
-            	   		System.out.println("Enter new Phone Number");
-           				p.setPhno(obj.next());
-           				break;
-            	   	case 8:
-            	   		System.out.println("Enter new Email");
-           				p.setEmail(obj.next());
-           				break;
-            	   }	
-             }
-      } 
-	
-}
+	}	
 }
