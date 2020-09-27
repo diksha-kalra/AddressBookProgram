3d01ab86fd5c0334147cfa7761453a3528e7ea4f
diksha-kalra <kalradiksha11@gmail.com>
2020-09-27 21:31:24 +0530

    Added: Printing Welcome Message

---
+++ AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.addressbook;
+import java.util.*;
+
+
+public class AddressBookMain{
+	
+	public static void main(String[] args) {	
+		
+		//Printing welcome message
+		System.out.println("Welcome To Address Book Program");
+		
+	}
+}
