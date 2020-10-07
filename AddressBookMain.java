520271109386812eae62e76f52e467af8a77e5de
diksha-kalra <kalradiksha11@gmail.com>
2020-10-08 01:41:36 +0530

    merging master with UC_7

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,6 @@
 package com.addressbook;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class AddressBookMain {
 
@@ -15,8 +14,6 @@ public class AddressBookMain {
 		while (true) {
 			System.out.println("Enter 1 to add addressbook");
 			System.out.println("Enter 2 to display address book");
-			System.out.println("Enter 3 to display person across address book on basis of city");
-			System.out.println("Enter 4 to display person across address book on basis of city");
 			System.out.println("Enter 0 to exit");
 			int ch1 = obj.nextInt();
 			if (ch1 == 1) {
@@ -29,13 +26,6 @@ public class AddressBookMain {
 					System.out.println("Enter 2 for viewing all contact details");
 					System.out.println("Enter 3 to modify details");
 					System.out.println("Enter 4 to delete details of a person");
-					System.out.println("Enter 5 to get persons of same city");
-					System.out.println("Enter 6 to get persons of same State");
-					System.out.println("Enter 7 to get count of person belonging to same city");
-					System.out.println("Enter 8 to get count of person belonging to same state");
-					System.out.println("Enter 9 to get sorted list of contacts by person name");
-					System.out.println("Enter 10 to get sorted list of city names");
-					System.out.println("Enter 11 to get sorted list of state names");
 					System.out.println("Enter 0 to exit");
 					System.out.println("Enter the action to perform");
 					int ch = obj.nextInt();
@@ -63,69 +53,12 @@ public class AddressBookMain {
 						System.out.println("Enter the name of person whose contact is to be deleted");
 						String name = obj.next();
 						contactPerson.remove(name);
-					} else if (ch == 5) {
-						System.out.println("Enter the city name");
-						String cityName = obj.next();
-						List<PersonInfo> personByCity = new ArrayList<PersonInfo>();
-						personByCity = (contactPerson.getPerson()).stream()
-								.filter(PersonInfo -> PersonInfo.getCity().equals(cityName))
-								.collect(Collectors.toList());
-						for (PersonInfo p : personByCity) {
-							System.out.println(p.getFirst_name());
-						}
-					} else if (ch == 6) {
-						System.out.println("Enter the state name");
-						String stateName = obj.next();
-						List<PersonInfo> personByState = new ArrayList<PersonInfo>();
-						personByState = (contactPerson.getPerson()).stream()
-								.filter(PersonInfo -> PersonInfo.getState().equals(stateName))
-								.collect(Collectors.toList());
-						for (PersonInfo p : personByState) {
-							System.out.println(p.getFirst_name());
-						}
-					} else if (ch == 7) {
-						System.out.println("Enter the city name");
-						String cityName = obj.next();
-						Long personByCity = (contactPerson.getPerson()).stream()
-								.filter(PersonInfo -> PersonInfo.getCity().equals(cityName)).count();
-						System.out.println("No of person in same city : " + personByCity);
-					} else if (ch == 8) {
-						System.out.println("Enter the state name");
-						String stateName = obj.next();
-						Long personByState = (contactPerson.getPerson()).stream()
-								.filter(PersonInfo -> PersonInfo.getState().equals(stateName)).count();
-						System.out.println("No of person in same city : " + personByState);
-					} else if (ch == 9) {
-						List<String> sortedByName = new ArrayList<>();
-						sortedByName = (contactPerson.getPerson()).stream().map(PersonInfo -> PersonInfo.toString())
-								.sorted().collect(Collectors.toList());
-						for (String p : sortedByName) {
-							System.out.println(p);
-						}
-					} else if (ch == 10) {
-						List<String> sortedByCity = new ArrayList<>();
-						sortedByCity = (contactPerson.getPerson()).stream().map(PersonInfo -> PersonInfo.getCity())
-								.sorted().collect(Collectors.toList());
-						for (String p : sortedByCity) {
-							System.out.println(p);
-						}
-					} else if (ch == 11) {
-						List<String> sortedByState = new ArrayList<>();
-						sortedByState = (contactPerson.getPerson()).stream().map(PersonInfo -> PersonInfo.getState())
-								.sorted().collect(Collectors.toList());
-						for (String p : sortedByState) {
-							System.out.println(p);
-						}
 					} else {
 						break;
 					}
 				}
 			} else if (ch1 == 2) {
 				address.viewAddressBook();
-			} else if (ch1 == 3) {
-				address.searchAddressBookByCity();
-			} else if (ch1 == 4) {
-				address.searchAddressBookByState();
 			} else {
 				break;
 			}
